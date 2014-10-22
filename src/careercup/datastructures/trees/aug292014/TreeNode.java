572a09eb4766bfefa9fea8129bece614d72298da
unknown <ralph.joachim@DSW003153.docusignhq.com>
2014-10-21 19:40:52 -0700

    initial repository

---
+++ src/careercup/datastructures/trees/aug292014/TreeNode.java
@@ -0,0 +1,11 @@
+package careercup.datastructures.trees.aug292014;
+
+public class TreeNode
+{
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int val){
+		this.val = val;
+	}	
+}
